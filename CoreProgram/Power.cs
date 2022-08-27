c188b05c222fde0ba3aded1217485d79c0598f18
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-27 19:24:53 +0530

    [Shivani] 3. table of Power of 2

---
+++ CoreProgram/Power.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CoreProgram
+{
+    internal class Power
+    {
+        public static void PrintTable()
+        {
+            const int POWER_OF = 2;
+            Console.WriteLine("Enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());
+          
+            for(int i = 1; i <= num; i++)
+            {
+                int result = POWER_OF * i;
+                Console.Write("{0} * {1} = ",POWER_OF,i);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- CoreProgram/Program.cs
+++ CoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace CoreProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Flip Coin\t 2. Leap Year");
+            Console.WriteLine("Enter Number\n 1. Flip Coin\t 2. Leap Year\t 3. Print Table");
             int num=Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -15,6 +15,9 @@ namespace CoreProgram
                     break;
                 case 2:
                     LeapYear.CheckLeapYear();
+                    break;
+                case 3:
+                    Power.PrintTable();
                     break;
                 default:
                     Console.WriteLine("Entered wrong number");
