c5736766aeb52e5f4f5bfc24f65847af066797d7
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-27 19:02:43 +0530

    [Shivani] 2. Leap year

---
+++ CoreProgram/LeapYear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    internal class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Enter year");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(year+" is a leap year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year+" is not a leap year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(year+" is a leap year");
+
+            }
+            else
+            {
+                Console.WriteLine(year+" is not a leap year");
+            }
+        }
+    }
+
+}
--- CoreProgram/Program.cs
+++ CoreProgram/Program.cs
@@ -6,12 +6,15 @@ namespace CoreProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Flip Coin\t");
+            Console.WriteLine("Enter Number\n 1. Flip Coin\t 2. Leap Year");
             int num=Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
                 case 1:
                     FlipCoin.PercentageHeadsTails();
+                    break;
+                case 2:
+                    LeapYear.CheckLeapYear();
                     break;
                 default:
                     Console.WriteLine("Entered wrong number");
