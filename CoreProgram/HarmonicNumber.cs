ba6713e9d71f03f016abcfeddb7c4faeb1a3a197
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-27 19:54:18 +0530

    [Shivani] 4. Harmonic Number

---
+++ CoreProgram/HarmonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    internal class HarmonicNumber
+    {
+        public static void HarmonicValue()
+        {
+            double num = 0;
+            Console.WriteLine("Enter the harmonic you want to find: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write("1 / {0} + ",i);
+                num += (1 / (float) i);
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Harmonic value {0} is : {1}",n,num);
+        }
+    }
+}
--- CoreProgram/Program.cs
+++ CoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace CoreProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number\n 1. Flip Coin\t 2. Leap Year\t 3. Print Table");
+            Console.WriteLine("Enter Number\n 1. Flip Coin\t 2. Leap Year\t 3. Print Table\t 4. Harmonic Number");
             int num=Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -18,6 +18,9 @@ namespace CoreProgram
                     break;
                 case 3:
                     Power.PrintTable();
+                    break;
+                case 4:
+                    HarmonicNumber.HarmonicValue();
                     break;
                 default:
                     Console.WriteLine("Entered wrong number");
