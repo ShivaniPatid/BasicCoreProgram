6d34ce3387bdd7800cd3c3671861b0d0f6ddebc4
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-28 11:52:33 +0530

    [Shivani] 7. Swap two numbers

---
+++ CoreProgram/Swapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CoreProgram
+{
+    internal class Swapping
+    {
+        public static void SwapTwoNumbers()
+        {
+            int number1, number2, temp;
+            Console.WriteLine("Enter the number1");
+            number1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ENter the number2");
+            number2= Convert.ToInt32(Console.ReadLine());
+
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("Afetr Swapping");
+            Console.WriteLine("number1 : "+number1+"\nnumber2 : "+number2);
+        }
+    }
+}
