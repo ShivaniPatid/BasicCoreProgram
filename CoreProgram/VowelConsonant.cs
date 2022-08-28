48e39ba047133b934a1ee44563e1c04c30d4734e
ShivaniPatid <shivanipatidar712@gmail.com>
2022-08-28 12:16:24 +0530

    [Shivani] 9. Check vowel or consonant

--- CoreProgram/VowelConsonant.cs
+++ CoreProgram/VowelConsonant.cs
@@ -8,15 +8,15 @@ namespace CoreProgram
         public static void CheckVowelConsonant()
         {
             Console.WriteLine("Enter any character");
-            char ch=Convert.ToChar(Console.ReadLine());
+            char ch = Convert.ToChar(Console.ReadLine());
 
             if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
             {
-                Console.WriteLine(ch+" character is a vowel");
+                Console.WriteLine(ch + " character is a vowel");
             }
             else
             {
-                Console.WriteLine(ch+" character is a consonant");
+                Console.WriteLine(ch + " character is a consonant");
             }
         }
     }
